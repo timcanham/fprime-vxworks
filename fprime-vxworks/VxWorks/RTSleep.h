fd4a13e8a1ccd7ba0d0df87836be2488a195b8ea
L <82917144+l198881@users.noreply.github.com>
2021-08-30 12:51:06 -0300

    Update RTSleep.h

--- fprime-vxworks/VxWorks/RTSleep.h
+++ fprime-vxworks/VxWorks/RTSleep.h
@@ -1,24 +1,24 @@
-#ifndef OS_RTSLEEP_H
-#define OS_RTSLEEP_H
+ OS_RTSLEEP_H
+ OS_RTSLEEP_H
 
-#include <time.h>
-#include <errno.h>
-#include <signal.h>
-#include <stdio.h>
+ <time.h>
+ <errno.h>
+ <signal.h>
+ <stdio.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+ __cplusplus
+ "C" {
+
 
 /* Sleep for msec milliseconds */
-void rt_sleep_ms(const long msec);
+     rt_sleep_ms(const long msec);
 /* Sleep for usec microseconds */
-void rt_sleep_us(const long usec);
+     rt_sleep_us(const long usec);
 /* Sleep for nsec nanoseconds */
-void rt_sleep_ns(const long nsec);
+     rt_sleep_ns(const long nsec);
 
-#ifdef __cplusplus
+     __cplusplus
 }
-#endif
 
-#endif
+
+
